e6d7b1b3e612400bffc7122bec01b2d62b368648
stacy.cheng <stacy.hy@medialand.tw>
2023-09-07 18:56:40 +0800

    test.

---
+++ src/components/ModalView.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="w-full fixed top-0 right-0 bottom-0 left-0 z-50" :class="!isShow && 'hidden'">
+    <div class="bg-b-1/50 absolute top-0 left-0 right-0 bottom-0"></div>
+    <div class="modal-container h-full text-center px-20px lg:px-60px leading-[100vh]">
+      <div class="modal-content inline-block align-middle relative rounded text-b-1 mx-auto leading-none">
+        <div> {{ msg }}</div>
+        <!-- @close="closeModal" -->
+        <!-- @click="$emit('close')" -->
+        <button class="close-btn absolute right-0 top-0 w-[40px] h-[40px] cursor-pointer" >
+          X
+        </button>
+      </div>
+    </div>
+
+
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+  },
+  isShow: {
+    type: Boolean,
+  }
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 <script setup>
-import { ref, computed } from 'vue';
+import { ref, reactive, computed } from 'vue';
+import ModalView from '../components/ModalView.vue';
 
 let id = 0;
 const newTask = ref(''); // 寫標題
@@ -9,7 +10,19 @@ const todoList = ref([
    { id: id++, task: 'No1', note: '12', status: false },
    { id: id++, task: 'No2', note: '2', status: false },
 ])
-
+const isShow = ref(false);
+//
+const test = reactive({
+    name: 'Test',
+    list: [
+      'Vue1',
+      'Vue2',
+      'Vue3'
+    ]
+});
+const publishedBooksMessage = computed(() => {
+  return test.list.length > 3 ? 'Yes' : 'No'
+})
 const filteredTodos = computed(() => {
   return isDone.value
     ? todoList.value.filter((item) => !item.status)
@@ -17,17 +30,26 @@ const filteredTodos = computed(() => {
 });
 // 新增任務
 function addTodo() {
-  todoList.value.push({ id: id++, task: newTask.value, note: newNote.value, status: false });
-  newTask.value = '';
-  newNote.value = '';
+  if(newTask.value) {
+    todoList.value.push({ id: id++, task: newTask.value, note: newNote.value, status: false });
+    newTask.value = '';
+    newNote.value = '';
+    isShow.value = false;
+    return;
+  }
+  isShow.value = true;
 }
 // 刪除任務
 function deleteTodo(ele) {
   todoList.value = todoList.value.filter((item) => item !== ele)
 }
 // 更新任務
-function updateTood() {
+// function updateTood() {
 
+// }
+//開關 Modal
+function closeModal() {
+  isShow.value = false;
 }
 </script>
 
@@ -42,15 +64,19 @@ function updateTood() {
       </div>
       <ul class="my-40px w-full">
         <li v-for="todo in filteredTodos" :key="todo.id" class="flex gap-2 py-2">
-          <input readonly class="flex-auto w-1/3" :v-model="todo.note" :value="todo.task" >
+          <input  class="flex-auto w-1/3" :v-model="todo.note" :value="todo.task" >
           <input readonly class="flex-auto w-1/3" :v-model="todo.note" :value="todo.note" >
           <div class="flex gap-2 items-center">
-            <button @click.prevent="updateTodo(todo)" class="py-2 px-4 bg-red-700 text-white rounded-sm">編輯</button>
+            <!-- <button @click.prevent="updateTodo(todo)" class="py-2 px-4 bg-red-700 text-white rounded-sm">編輯</button> -->
             <button @click.prevent="deleteTodo(todo)" class="py-2 px-4 bg-b-1 text-white rounded-sm">刪除</button>
           </div>
         </li>
       </ul>
       </div>
     </section>
+    <section class="py-60px">
+        <h1>{{ publishedBooksMessage }}</h1>
+    </section>
+    <ModalView :msg="'任務要填寫喔'" />
   </main>
 </template>
